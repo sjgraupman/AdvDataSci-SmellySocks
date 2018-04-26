14fdb3d8a31dc826a9b10081423072371cde304e
Sarah Graupman <sarah.graupman@marquette.edu>
2018-04-26 18:24:28 -0500

    Make graph a scatter plot and responsive

--- Website/js/d3-script.js
+++ Website/js/d3-script.js
@@ -87,7 +87,6 @@ queue()
 		.style("stroke", "white")
 		.style('stroke-width', 0.3)
 		.on('mouseover', function (d) {
-			//console.log(d);
 			tip.show(d);
 
 			d3.select(this)
@@ -184,8 +183,9 @@ noUiSlider.create(handlesSlider, {
 
 // 2. Use the margin convention practice
 var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-	, width = 960 - margin.left - margin.right // Use the window's width
-	, height = 550 - margin.top - margin.bottom; // Use the window's height
+	//Update padding with new margins
+	, width = width - margin.left - margin.right 	
+	, height = height - margin.top - margin.bottom; 
 
 // The number of datapoints
 var n = 100;
@@ -201,18 +201,22 @@ var yScale = d3.scaleLinear()
 	.range([height, 0]); // output
 
 // 7. d3's line generator
-var line = d3.line()
+/*var line = d3.line()
 	.x(function (d, i) { return xScale(i); }) // set the x values for the line generator
 	.y(function (d) { return yScale(d.y); }) // set the y values for the line generator
 	.curve(d3.curveMonotoneX) // apply smoothing to the line
+	*/
 
 // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
 var dataset = d3.range(n).map(function (d) { return { "y": d3.randomUniform(1)() } })
 
 // 1. Add the SVG to the page and employ #2
 var svg2 = d3.select("#smellgraph").append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
+	.classed("svg-container", true) //container class to make it responsive
+	.attr("preserveAspectRatio", "xMinYMin meet")
+	.attr("viewBox", "0 0 600 400")
+//	.attr("width", width + margin.left + margin.right)
+//	.attr("height", height + margin.top + margin.bottom)
 	.append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -228,10 +232,11 @@ svg2.append("g")
 	.call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
 
 // 9. Append the path, bind the data, and call the line generator
-svg2.append("path")
+/*svg2.append("path")
 	.datum(dataset) // 10. Binds data to the line
 	.attr("class", "line") // Assign a class for styling
 	.attr("d", line); // 11. Calls the line generator
+	*/
 
 // 12. Appends a circle for each datapoint
 svg2.selectAll(".dot")
